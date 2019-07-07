93d7b983d70827efabc76e468dc33b0bd454247c
danielpav <danielpav@gmail.com>
2019-07-06 17:15:32 -0700

    adds word count exercise

---
+++ wc.c
@@ -0,0 +1,22 @@
+#define yes = 1
+#define no = 0
+
+main() 
+{
+	int c, nl, nw, nc, inword;
+
+	inword = NO;
+	nc = nw = nl = 0;
+	while ((c = getchar()) != EOF) {
+		++nc;
+		if (c == '\n')
+			++nl;
+		if ( c == ' ' || c == '\t' || c == '\n' )
+			inword = NO;
+		else if (inword == NO){
+			inword = YES;
+			++nw;
+		}
+	}
+	printf("%d %d %d\n", nl, nw, nc);
+}
